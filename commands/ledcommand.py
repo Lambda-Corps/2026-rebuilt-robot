1a872ca6e945813a6334e0fad12e8162becf848e
wilson roth <wilson116693@mcpsva.org>
2026-02-21 11:48:04 -0500

    rainbowcode

--- commands/ledcommand.py
+++ commands/ledcommand.py
@@ -1,15 +1,17 @@
 import wpilib
 from commands2 import Command
 from subsystems.ledsubsystem import LEDSubsystem
+from subsystems.shooter import Shooter
+from subsystems.intake import Intake
 
 class LEDCommand(Command):
-#    def __init__(self, led: LEDSubsystem, controller: wpilib.Joystick) -> None:
-    def __init__(self, led: LEDSubsystem, tempControlValue : float) -> None:
+
+    def __init__(self, led: LEDSubsystem, shooter: Shooter, Intake: Intake ) -> None:
         super().__init__()
 
         self.led = led
-    #    self.controller = controller
-        self.tempControlValue = tempControlValue
+        self.shooter = shooter
+        self.intake = Intake
       
         self.addRequirements(led)
 
@@ -17,13 +19,7 @@ class LEDCommand(Command):
         pass       #  This function is not being used.
 
     def execute(self) -> None:
-    #    Xaxis = self.controller.getRawAxis(0)
-    #    Yaxis = self.controller.getRawAxis(1)
-
-        Xaxis = self.tempControlValue
-        Yaxis = self.tempControlValue
-   
-        self.led.joystickControlsColor(Xaxis,Yaxis)
+        self.led.rainbow()
 
     def end(self, interrupted: bool) -> None:
        pass       #  This function is not being used.
